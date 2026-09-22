c09c547e4bde16e41304e60f9c36f9b11b20fab4
MarfinCLX <glebtkacov5@gmail.com>
2026-09-22 20:49:52 +0100

    Added a HTML button and JS logic to reset name

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 const form = document.querySelector('form');
 const inputWrapper = document.querySelector('.input-wrapper');
 const input = document.querySelector('.input');
-const submitBtn = document.querySelector('.submit-btn');
+const submitNameBtn = document.querySelector('.submit-btn');
+const resetNameBtn = document.querySelector('.reset-name-btn');
+
+const updateButtonState = (button, isDisabled) => {
+    button.disabled = isDisabled;
+    button.classList.toggle('disabled-btn', isDisabled);
+};
+
+resetNameBtn.disabled = true;
+resetNameBtn.classList.toggle('disabled-btn', resetNameBtn.disabled);
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    input.disabled = true;
-    submitBtn.disabled = true;
-
     const playerName = input.value;
 
-    if (input.disabled) {
-        inputWrapper.classList.add('input-locked');
-    } else {
-        inputWrapper.classList.remove('input-locked');
-    }
+    input.disabled = true;
+    inputWrapper.classList.add('input-locked');
 
-    if (submitBtn.disabled) {
-        submitBtn.classList.add('disabled-btn');
-    } else {
-        submitBtn.classList.remove('disabled-btn');
-    }
+    updateButtonState(submitNameBtn, true);
+    updateButtonState(resetNameBtn, false);
 
     //  if (input.validity.typeMismatch) {
     // input.setCustomValidity("I am expecting an email address!");
     // } else {
     // input.setCustomValidity("");
     // }
-    
-    console.log(playerName)
-})
+
+    console.log(playerName);
+});
+
+resetNameBtn.addEventListener('click', () => {
+    console.clear();
+    input.value = '';
+    input.disabled = false;
+    inputWrapper.classList.remove('input-locked');
+
+    updateButtonState(submitNameBtn, false);
+    updateButtonState(resetNameBtn, true);
+
+    input.focus();
+});
