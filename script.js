fabfc99c611f0a72bcdf0a338299244e646aa114
MarfinCLX <glebtkacov5@gmail.com>
2026-09-26 16:30:11 +0100

    Added a structure to check winning combinations on game

--- script.js
+++ script.js
@@ -77,6 +77,20 @@ function createGameBoard () {
         clearBoard
     }
 }
-function player (name, operator) {
-    return name, operator;
+
+function playerName (name, operator) {
+    return { name, operator };
 }
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const checkCombinations = winningCombinations.some(e => board[e[0]] !== "" && board[e[0]] === board[e[1]] && board[e[1]] === board[e[2]]);
